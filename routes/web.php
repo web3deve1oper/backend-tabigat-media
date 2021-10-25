d6e2b25f52c1ba4f6402dd199e1d5c5d5bf5a60c
sabadoryo <eabilbay@gmail.com>
2021-10-25 10:11:58 +0300

    fixes

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('',function() {
+   return 'hello';
+});
+
 Route::group(['prefix' => 'admin'], function () {
 
     Route::post('/login', [\App\Http\Controllers\Admin\AuthController::class, 'login']);
